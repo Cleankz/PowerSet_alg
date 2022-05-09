e154b053354e56edf7abc61e2bc224e18323e42e
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-05-09 21:36:30 +0300

    Update ps_alg.py

--- ps_alg.py
+++ ps_alg.py
@@ -1,5 +1,6 @@
 # наследуйте этот класс от HashTable
 # или расширьте его методами из HashTable
+import random
 class PowerSet:
 
     def __init__(self):
@@ -35,27 +36,27 @@ class PowerSet:
     def intersection(self, set2):
         result_set = []
         for i in range(len(self.array)):
-            if self.array[i] in set2:
-                result_set.append(self.array[i])
+            if self.array[i] in set2.array:
+                result_set.put(self.array[i])
         # пересечение текущего множества и set2 (self.array[i])
         return result_set
 
     def union(self, set2):
         result_set = self.array
-        for i in set2:
+        for i in set2.array:
             result_set.append(i)
         return self.array
 
     def difference(self, set2):
         result_set = []
         for i in range(len(self.array)):
-            if self.array[i] not in set2:
+            if self.array[i] not in set2.array:
                 result_set.append(self.array[i])
         # разница текущего множества и set2
         return result_set
 
     def issubset(self, set2):
-        for i in (set2):
+        for i in (set2.array):
             if i not in self.array:
                 return False
         return True
