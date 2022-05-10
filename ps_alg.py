d2bc12dae0dce17c9135550d9132545bfd556bd3
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-05-10 16:38:42 +0300

    Update ps_alg.py

--- ps_alg.py
+++ ps_alg.py
@@ -1,6 +1,8 @@
 # наследуйте этот класс от HashTable
 # или расширьте его методами из HashTable
 import random
+
+from regex import P
 class PowerSet:
 
     def __init__(self):
@@ -52,12 +54,32 @@ class PowerSet:
         return result_set
 
     def union(self, set2):
-        result_set = self.array
+        t1 = type({123,156})
+        t2 = type([1,2,3])
+        t3 = type({})
+        if type(set2) is t1 or type(set2) is t2 or type(set2) is t3:
+            result_set = PowerSet()
+            result_set.array = self.array
+            for i in set2:
+                result_set.put(i)
+            return result_set
+
+        result_set = PowerSet()
+        result_set.array = self.array
         for i in set2.array:
-            result_set.append(i)
-        return self.array
+            result_set.put(i)
+        return result_set
 
     def difference(self, set2):
+        t1 = type({123,156})
+        t2 = type([1,2,3])
+        t3 = type({})
+        if type(set2) is t1 or type(set2) is t2 or type(set2) is t3:
+            result_set = PowerSet()
+            for i in self.array:
+                if i not in set2:
+                    result_set.put(i)
+            return result_set
         result_set = PowerSet()
         for i in range(len(self.array)):
             if self.array[i] not in set2.array:
@@ -66,6 +88,15 @@ class PowerSet:
         return result_set
 
     def issubset(self, set2):
+        t1 = type({123,156})
+        t2 = type([1,2,3])
+        t3 = type({})
+        if type(set2) is t1 or type(set2) is t2 or type(set2) is t3:
+            for i in set2:
+                if i not in self.array:
+                    return False
+            return True
+
         for i in (set2.array):
             if i not in self.array:
                 return False
@@ -74,11 +105,12 @@ class PowerSet:
         # подмножество текущего множества,
         # иначе False
 # a = PowerSet()
-# b = [1235,46551651,51,51,6156,21,54,2,12231,5,12,5,42,6442,26,412,4,78,96,24,67,23,25,96,84,15,19]
+# # b = [1235,46551651,51,51,6156,21,54,2,12231,5,12,5,42,6442,26,412,4,78,96,24,67,23,25,96,84,15,19]
 # b = {}
-# b = PowerSet()
-# for i in range(50):
-#     a.put(random.randint(0,50))
-# for i in range(50):
-#     b.put(random.randint(0,50))
-# print(a.intersection(b))
+# # b = PowerSet()
+# for i in range(51,100):
+#     a.put(i)
+# # for i in range(50):
+# #     b.put(random.randint(0,50))
+# result = a.union(b)
+# print(result.size())
