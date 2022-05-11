9ce832f8810bfc5eea86a4087037a54b5fd4feb7
Cleankz <92687535+Cleankz@users.noreply.github.com>
2022-05-11 13:58:28 +0300

    Update ps_alg.py

--- ps_alg.py
+++ ps_alg.py
@@ -31,7 +31,6 @@ class PowerSet:
         # возвращает True если value удалено
         # иначе False
 
-
     def intersection(self, set2):
         if isinstance(set2, (list, dict, set)): # на всякий случай добавил, для своего спокойствия, чтобы точно знать что я учел все варианты 
             result_set = PowerSet()
@@ -44,9 +43,8 @@ class PowerSet:
         for i in range(len(self.array)):
             if self.array[i] in set2.array: # если элемент есть и в множестве-параметре тоже то мы  добавляем его к результату
                 result_set.put(self.array[i])
-        # пересечение текущего множества и set2 (self.array[i])
-        return result_set # возвращаем  новое множество
 
+        return result_set # возвращаем  новое множество
 
     def union(self, set2):
         if isinstance(set2, (list, dict, set)): # на всякий случай добавил, для своего спокойствия, чтобы точно знать что я учел все варианты
@@ -67,21 +65,23 @@ class PowerSet:
             return set2
         for i in set2.array:
             result_set.put(i)
+
         return result_set # возвращаем новое множество содержащее в себе все элементы двух других без повторений
 
 
-    def difference(self, set2):
+    def difference(self, set2):        # разница текущего множества и set2
         if isinstance(set2, (list, dict, set)): # на всякий случай добавил, для своего спокойствия, чтобы точно знать что я учел все варианты
             result_set = PowerSet()
             for i in self.array:
                 if i not in set2:
                     result_set.put(i)
             return result_set
+
         result_set = PowerSet() # создаем новое множество
         for i in range(len(self.array)):
             if self.array[i] not in set2.array: # если элемента нет в множ-параметре то мы его добавляем в результат
                 result_set.put(self.array[i])
-        # разница текущего множества и set2
+
         return result_set # возвращаем новое множество
 
 
@@ -97,7 +97,7 @@ class PowerSet:
         if len(set2.array) <= 0: # так как пустое множество является подмножеством любого множества 
             return True
 
-        for i in (set2.array): 
+        for i in (set2.array):
             if i not in self.array:#если хотябы один элемент не входит в основное множество то False
                 return False
         return True
